f9e43fc295834d2c9d09948b410f893cd7dd0091
kimich <ickim1218@gmail.com>
2024-07-22 15:42:32 +0900

    feat: user entity 구현

---
+++ src/main/java/com/example/delivery/domain/user/entity/User.java
@@ -0,0 +1,40 @@
+package com.example.delivery.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 45)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 45)
+    private String phone;
+
+    @Column(nullable = false, length = 45)
+    private String address;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
