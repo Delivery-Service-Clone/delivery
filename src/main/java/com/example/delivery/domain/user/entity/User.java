2400fb0f125cec377a625c6b278d7e6853175b08
kimich <ickim1218@gmail.com>
2024-07-22 15:49:03 +0900

    fix: google-java-format 적용

--- src/main/java/com/example/delivery/domain/user/entity/User.java
+++ src/main/java/com/example/delivery/domain/user/entity/User.java
@@ -13,28 +13,28 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false, unique = true, length = 45)
-    private String email;
+  @Column(nullable = false, unique = true, length = 45)
+  private String email;
 
-    @Column(nullable = false, length = 45)
-    private String name;
+  @Column(nullable = false, length = 45)
+  private String name;
 
-    @Column(nullable = false, length = 255)
-    private String password;
+  @Column(nullable = false, length = 255)
+  private String password;
 
-    @Column(nullable = false, length = 45)
-    private String phone;
+  @Column(nullable = false, length = 45)
+  private String phone;
 
-    @Column(nullable = false, length = 45)
-    private String address;
+  @Column(nullable = false, length = 45)
+  private String address;
 
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
+  @Column(nullable = false)
+  private LocalDateTime createdAt;
 
-    @Column(nullable = false)
-    private LocalDateTime updatedAt;
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
 }
