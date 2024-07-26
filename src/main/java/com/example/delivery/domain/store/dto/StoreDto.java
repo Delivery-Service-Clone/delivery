f5e0e1883c9aa1163d7b962e9a4346febc73c86d
kimich <ickim1218@gmail.com>
2024-07-26 09:39:32 +0900

    feat: 예외 적용

--- src/main/java/com/example/delivery/domain/store/dto/StoreDto.java
+++ src/main/java/com/example/delivery/domain/store/dto/StoreDto.java
@@ -1,5 +1,6 @@
 package com.example.delivery.domain.store.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class StoreDto {
@@ -7,11 +8,17 @@ public class StoreDto {
   @Getter
   public static class request {
 
+    @NotNull
     private Long ownerId;
+    @NotNull
     private String storeName;
+    @NotNull
     private String storePhone;
+    @NotNull
     private String storeAddress;
+    @NotNull
     private String openStatus;
+    @NotNull
     private String introduction;
   }
 }
