dbb2c5d895c3abd83a96e2f2b21d235dca2f99f0
kimich <ickim1218@gmail.com>
2024-07-27 18:31:45 +0900

    feat: spring-docs 세팅

--- src/main/java/com/example/delivery/domain/store/controller/StoreController.java
+++ src/main/java/com/example/delivery/domain/store/controller/StoreController.java
@@ -5,6 +5,8 @@ import static com.example.delivery.global.result.ResultCode.STORE_REGISTRATION_S
 import com.example.delivery.domain.store.dto.request.StoreCreateDto;
 import com.example.delivery.domain.store.service.StoreService;
 import com.example.delivery.global.result.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "가게", description = "가게 관련 api 입니다")
 public class StoreController {
 
   private final StoreService storeService;
 
   @PostMapping("/stores")
+  @Operation(summary = "가게 등록", description = "새로운 가게를 등록한다.")
   public ResponseEntity<ResultResponse> registerStore(
       @Valid @RequestBody StoreCreateDto storeCreateDto) {
 
