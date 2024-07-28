74ada0e68c429d28a09fab1dc4b2999ed07d7dc6
조성현 <agllwy116@naver.com>
2024-07-28 12:46:26 +0900

    fix: 엔티티 이름 변경

--- src/main/java/com/example/delivery/domain/user/entity/Member.java
+++ src/main/java/com/example/delivery/domain/user/entity/Member.java
@@ -3,23 +3,27 @@ package com.example.delivery.domain.user.entity;
 import com.example.delivery.domain.order.entity.Order;
 import com.example.delivery.global.common.BaseEntity;
 import jakarta.persistence.*;
+import java.util.Collection;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "users")
-public class User extends BaseEntity {
+@Table(name = "members")
+public class Member extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @OneToMany(
-      mappedBy = "user",
+      mappedBy = "member",
       fetch = FetchType.LAZY,
       cascade = CascadeType.ALL,
       orphanRemoval = true)
@@ -39,4 +43,13 @@ public class User extends BaseEntity {
 
   @Column(nullable = false, length = 45)
   private String address;
+
+  @Builder
+  public Member(String email, String name, String password, String phone, String address) {
+    this.email = email;
+    this.name = name;
+    this.password = password;
+    this.phone = phone;
+    this.address = address;
+  }
 }
