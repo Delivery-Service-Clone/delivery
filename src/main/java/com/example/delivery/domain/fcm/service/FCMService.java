9e629fae9b0886859d1b842f7fd466660b8a770b
kimich <ickim1218@gmail.com>
2024-08-14 13:44:58 +0900

    fix: 라이더에 메시지 전송 domain 변경

--- src/main/java/com/example/delivery/domain/fcm/service/FCMService.java
+++ src/main/java/com/example/delivery/domain/fcm/service/FCMService.java
@@ -1,17 +1,12 @@
 package com.example.delivery.domain.fcm.service;
 
 import com.example.delivery.domain.fcm.dto.PushsRequestDto;
-import com.example.delivery.domain.rider.entity.Rider;
-import com.example.delivery.domain.rider.exception.RiderNotFoundByAddressException;
-import com.example.delivery.domain.rider.repository.RiderRepository;
+import com.example.delivery.domain.rider.dao.DeliveryDao;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
-import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,41 +15,18 @@ import org.springframework.stereotype.Service;
 public class FCMService {
 
   private final FirebaseMessaging firebaseMessaging;
-  private final RiderRepository riderRepository;
-
-  //  public void sendPush(PushRequestDto pushRequestDto) throws FirebaseMessagingException {
-  //
-  //    firebaseMessaging.send(makeMessage(userInfo.getUserPushToken(), pushRequestDto.getTitle(),
-  //        pushRequestDto.getContent()));
-  //  }
+  private final DeliveryDao deliveryDao;
 
   public void sendPushs(PushsRequestDto pushsRequestDto) throws FirebaseMessagingException {
 
-    // 1. 주소를 기반으로 라이더 조회
-    List<Rider> riders = riderRepository.findByAddress(pushsRequestDto.getAddress());
-    if (riders.isEmpty()) {
-      throw new RiderNotFoundByAddressException();
-    }
+    Set<String> tokens = deliveryDao.getRiderTokensByAddress(pushsRequestDto.getAddress());
 
-    // 2. 라이더의 FCM 토큰 목록 추출
-    List<String> targetUserTokens =
-        riders.stream()
-            .map(Rider::getFcmToken)
-            .filter(Objects::nonNull)
-            .collect(Collectors.toList());
-
-    // 3. 푸시 메시지 전송
     MulticastMessage message =
-        makeMessages(pushsRequestDto.getTitle(), pushsRequestDto.getContent(), targetUserTokens);
+        makeMessages(pushsRequestDto.getTitle(), pushsRequestDto.getContent(), tokens);
     firebaseMessaging.sendMulticast(message);
   }
 
-  private Message makeMessage(String targetToken, String title, String body) {
-    Notification notification = Notification.builder().setTitle(title).setBody(body).build();
-    return Message.builder().setNotification(notification).setToken(targetToken).build();
-  }
-
-  private MulticastMessage makeMessages(String title, String body, List<String> targetTokens) {
+  private MulticastMessage makeMessages(String title, String body, Set<String> targetTokens) {
     Notification notification = Notification.builder().setTitle(title).setBody(body).build();
     return MulticastMessage.builder()
         .setNotification(notification)
