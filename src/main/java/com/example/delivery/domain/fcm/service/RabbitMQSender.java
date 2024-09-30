9af9a0f64d0f19a3f81ebf986d6a89c45e9f9d7d
kimich <ickim1218@gmail.com>
2024-09-30 19:35:54 +0900

    feat: rabbit mq 설정 구현

---
+++ src/main/java/com/example/delivery/domain/fcm/service/RabbitMQSender.java
@@ -0,0 +1,26 @@
+package com.example.delivery.domain.fcm.service;
+
+import com.example.delivery.domain.fcm.dto.PushRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitMQSender {
+
+  private final RabbitTemplate rabbitTemplate;
+
+  @Value("${rabbitmq.exchange.name}")
+  private String exchangeName;
+
+  @Value("${rabbitmq.routing.key}")
+  private String routingKey;
+
+  public void send(PushRequestDto message) {
+    rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
+    System.out.println("Sent message: " + message.getContent());
+  }
+}
